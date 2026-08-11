2308724aded69993285f14ed71b69465d780eb90
Divyansh Sharma <divyanshsharma4507@gmail.com>
2026-08-11 18:28:27 +0530

    Refactor server initialization and connection handling in main function

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,47 @@
+use std::error;
 use::std::net::TcpListener;
+use::std::io::Read;
 
 fn main(){
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
-    println!("Velocity server started on port 6379");
+    let listener = match TcpListener::bind("127.0.0.1:6379"){
+        Ok(listener) => listener,
+        Err(error) => {
+            eprintln!("Failed to bind to 127.0.0.1:6379: {}", error);
+            return;
+        }
+    };
+
+    println!("Server is listening on 127.0.0.1:6379");
 
     loop{
-        let (stream, address) = listener.accept().unwrap();
-        println!("Client connected, {}", address);
+        let (mut stream, address) = match listener.accept(){
+            Ok(connection) => connection,
+            Err(error) => {
+                eprintln!("Failed to accept connection {}", error);
+                continue;
+            }
+        };
+        
+        println!("Client connected: {}", address);
+
+        let mut buffer = [0u8; 1024];
+
+        let bytes_read = match stream.read(&mut buffer){
+            Ok(0) => {
+                println!("Client Disconnected");
+                continue;
+            },
+
+            Ok(bytes) => bytes,
+
+            Err(error) => {
+                eprintln!("Failed to read from the client: {}", {error});
+                continue;
+            }
+        };
+
+        println!("Recieved {} bytes", bytes_read);
+
     }
 }
