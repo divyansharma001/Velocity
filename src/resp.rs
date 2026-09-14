4741cb8c794fd03f79ecfcd8bbbad5988b8e4db0
Divyansh Sharma <divyanshsharma4507@gmail.com>
2026-09-14 14:40:42 +0530

    Implement parse_bulk_string function with error handling and logging

--- src/resp.rs
+++ src/resp.rs
@@ -1,5 +1,6 @@
 use core::error;
-use std::os::unix::fs::PermissionsExt;
+
+use crate::resp::RespValue::BulkString;
 
 #[derive(Debug)]
 pub enum RespValue{
@@ -50,7 +51,49 @@ pub fn encode_array(values: &[RespValue]) -> Vec<u8> {
 }
 
 fn parse_bulk_string(data : &[u8], start: usize) -> Result<(RespValue, usize), String> {
-    todo!()
+    let bulk_start = start;
+
+    println!("Bulk starts at index: {}", bulk_start);
+    println!("Bulk type byte: {:?}", data[bulk_start]);
+
+    let bulk_length_end = data[bulk_start..]
+                                .windows(2)
+                                .position(|window| window == b"\r\n");
+
+    match bulk_length_end {
+        Some(length_end) => {
+            let length_bytes = &data[bulk_start+1..bulk_start+length_end];
+
+            let length_string = match std::str::from_utf8(length_bytes) {
+                Ok(value) => value,
+                Err(error) => {
+                    return Err(error.to_string());
+                }
+            };
+
+            let length = match length_string.parse::<usize>() {
+                Ok(value) => value,
+                Err(error) => {
+                     return Err(error.to_string());
+                }
+            };
+
+            let data_start = bulk_start+length_end+2;
+
+            let bulk_data = &data[data_start..data_start+length];
+
+            let bulk_string = RespValue::BulkString(bulk_data.to_vec());
+
+            let next_position = data_start+length+2;
+
+            Ok((bulk_string, next_position))
+        }
+        None => {
+            Err("Bulk length CRLF not found".to_string())
+        }
+    }
+
+                           
 }
 
 pub fn parse(data : &[u8]) -> Result<RespValue, String> {
@@ -86,63 +129,21 @@ pub fn parse(data : &[u8]) -> Result<RespValue, String> {
 
                     let bulk_start = position+2;
 
-                    println!("Bulk starts at index: {}", bulk_start);
-                    println!("Bulk type byte: {:?}", data[bulk_start]);
+                    let result = parse_bulk_string(data, bulk_start);
 
-                    let bulk_length_end = data[bulk_start..]
-                                              .windows(2)
-                                              .position(|window| window == b"\r\n" );
-
-                    match bulk_length_end {
-                          Some(length_end) => {
-                            let length_bytes = &data[bulk_start+1..bulk_start+length_end];
-                            let length_string = match std::str::from_utf8(length_bytes) {
-                                Ok(value) => value,
-                                Err(error) => {
-                                    println!("Invalid UTF-8: {}", error);
-                                    return Err(error.to_string());
-                                }
-                            };
-
-                            println!("Bulk length string: {}", length_string);
-
-                            let length = match length_string.parse::<usize>(){
-                                Ok(value) => value,
-                                Err(error) => {
-                                    println!("Invalid bulk string length {:?}", error);
-                                    return Err(error.to_string());
-                                }
-                            };
-
-                            println!("Bulk string length: {}", length);
-
-                            let data_start = bulk_start + length_end + 2;
-
-                            println!("Data starts at index: {}", data_start);
-
-                            let bulk_data = &data[data_start..data_start+length];
-
-                            println!("Bulk data: {:?}", bulk_data);
-
-                            let next_position = data_start+length+2;
-
+                    match result {
+                        Ok((bulk_string, next_position)) => {
+                            println!("Parsed bulk string: {:?}", bulk_string);
                             println!("Next position: {}", next_position);
 
-                            let bulk_string = RespValue::BulkString(bulk_data.to_vec());
-
-                            println!("BulkString: {:?}", bulk_string);
-
                             let array = RespValue::Array(vec![bulk_string]);
-
-                            return Ok(array);
-
-                          }
-
-                          None => {
-                            println!("Bulk length CRLF not found");
-                            return Err("Bulk length CRLF not found".to_string());
-                          }
+                            Ok(array)
                         }
+
+                        Err(error) => {
+                            Err(error)
+                        }
+                    }
 
                 }
 
